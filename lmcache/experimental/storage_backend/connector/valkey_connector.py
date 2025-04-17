68a91b576a14b50f2acf37d849b86ffa82f51616
baoloongmao <baoloongmao@tencent.com>
2025-04-17 19:39:03 +0800

    Profile redis connector

--- lmcache/experimental/storage_backend/connector/valkey_connector.py
+++ lmcache/experimental/storage_backend/connector/valkey_connector.py
@@ -16,8 +16,8 @@ import asyncio
 import os
 from typing import List, Optional, Tuple, Union, no_type_check
 
-import redis
-from redis import Redis
+import valkey
+from valkey import Valkey
 
 from lmcache.experimental.memory_management import (MemoryAllocatorInterface,
                                                     MemoryObj)
@@ -29,25 +29,20 @@ from lmcache.utils import CacheEngineKey
 
 logger = init_logger(__name__)
 
-# TODO(Jiayi): Use `redis.asyncio`
-# NOTE(Jiayi): `redis-py` supports async operations, but data copy
-# cannot be avoided. `hiredis` is more lower-level but asyncio is
-# not supported.
 
-
-class BaseRedisConnector(RemoteConnector):
-    """Base Redis connector with common operations"""
+class BaseValkeyConnector(RemoteConnector):
+    """Base Valkey connector with common operations"""
 
     def __init__(self, memory_allocator: MemoryAllocatorInterface):
         self.memory_allocator = memory_allocator
 
     @property
-    def read_client(self) -> Redis:
+    def read_client(self) -> Valkey:
         """Client for read operations (to be implemented by subclasses)"""
         raise NotImplementedError
 
     @property
-    def write_client(self) -> Redis:
+    def write_client(self) -> Valkey:
         """Client for write operations (to be implemented by subclasses)"""
         raise NotImplementedError
 
@@ -59,37 +54,37 @@ class BaseRedisConnector(RemoteConnector):
         if combined_bytes is None:
             return None
 
-        redis_metadata = RedisMetadata.deserialize(
+        valkey_metadata = RedisMetadata.deserialize(
             memoryview(combined_bytes[:METADATA_BYTES_LEN]))
         kv_bytes = combined_bytes[METADATA_BYTES_LEN:METADATA_BYTES_LEN +
-                                  redis_metadata.length]
+                                  valkey_metadata.length]
 
         memory_obj = self.memory_allocator.allocate(
-            redis_metadata.shape,
-            redis_metadata.dtype,
-            redis_metadata.fmt,
+            valkey_metadata.shape,
+            valkey_metadata.dtype,
+            valkey_metadata.fmt,
         )
         if memory_obj is None:
             logger.warning("Failed to allocate memory during remote receive")
             return None
 
         view = memoryview(memory_obj.byte_array)
-        view[:redis_metadata.length] = kv_bytes
+        view[:valkey_metadata.length] = kv_bytes
         return memory_obj
 
     async def put(self, key: CacheEngineKey, memory_obj: MemoryObj):
         key_str = key.to_string()
         kv_bytes = memory_obj.byte_array
-        redis_metadata = RedisMetadata(len(kv_bytes), memory_obj.get_shape(),
-                                       memory_obj.get_dtype(),
-                                       memory_obj.get_memory_format())
+        valkey_metadata = RedisMetadata(len(kv_bytes), memory_obj.get_shape(),
+                                        memory_obj.get_dtype(),
+                                        memory_obj.get_memory_format())
 
-        combined_bytes = redis_metadata.serialize() + kv_bytes
+        combined_bytes = valkey_metadata.serialize() + kv_bytes
         try:
             self.write_client.set(key_str, combined_bytes)
         except Exception as e:
             logger.error(f"Failed to put key {key_str},"
-                         f"meta type: {type(redis_metadata)},"
+                         f"meta type: {type(valkey_metadata)},"
                          f"data: {type(kv_bytes)}: {e}")
 
         self.memory_allocator.ref_count_down(memory_obj)
@@ -100,58 +95,50 @@ class BaseRedisConnector(RemoteConnector):
         pass
 
 
-class RedisConnector(BaseRedisConnector):
-    """Single-node Redis connector"""
+class ValkeyConnector(BaseValkeyConnector):
+    """Single-node Valkey connector"""
 
     def __init__(self, host: str, port: int, loop: asyncio.AbstractEventLoop,
                  memory_allocator: MemoryAllocatorInterface):
         super().__init__(memory_allocator)
-        self._client = Redis(host=host, port=port, decode_responses=False)
+        self._client = Valkey(host=host, port=port, decode_responses=False)
         self.loop = loop
 
     @property
-    def read_client(self) -> Redis:
+    def read_client(self) -> Valkey:
         return self._client
 
     @property
-    def write_client(self) -> Redis:
+    def write_client(self) -> Valkey:
         return self._client
 
     async def close(self):
         self._client.close()
 
 
-class RedisSentinelConnector(BaseRedisConnector):
-    """Redis Sentinel connector"""
+class ValkeySentinelConnector(BaseValkeyConnector):
+    """Valkey Sentinel connector"""
 
-    ENV_REDIS_TIMEOUT = "REDIS_TIMEOUT"
-    ENV_REDIS_SERVICE_NAME = "REDIS_SERVICE_NAME"
+    ENV_VALKEY_TIMEOUT = "VALKEY_TIMEOUT"
+    ENV_VALKEY_SERVICE_NAME = "VALKEY_SERVICE_NAME"
 
     def __init__(self, hosts_and_ports: List[Tuple[str, Union[str, int]]],
                  loop: asyncio.AbstractEventLoop,
                  memory_allocator: MemoryAllocatorInterface):
         super().__init__(memory_allocator)
         # Get service name
-        match os.environ.get(self.ENV_REDIS_SERVICE_NAME):
+        match os.environ.get(self.ENV_VALKEY_SERVICE_NAME):
             case None:
                 logger.warning(
-                    f"Environment variable {self.ENV_REDIS_SERVICE_NAME} is not"
-                    f"found, using default value 'redismaster'")
-                service_name = "redismaster"
+                    f"Environment variable {self.ENV_VALKEY_SERVICE_NAME} is"
+                    f" not found, using default value 'valkeymaster'")
+                service_name = "valkeymaster"
             case value:
                 service_name = value
-
-        timeout: float = -1000.0
-
-        # Get timeout
-        match os.environ.get(self.ENV_REDIS_TIMEOUT):
-            case None:
-                timeout = 1
-            case value:
-                timeout = float(value)
-
+        timeout = float(os.environ.get(self.ENV_VALKEY_TIMEOUT, 1))
         logger.info(f"Host and ports: {hosts_and_ports}")
-        self.sentinel = redis.Sentinel(hosts_and_ports, timeout)
+
+        self.sentinel = valkey.Sentinel(hosts_and_ports, timeout)
         # FIXME(maobaolong):  _master would be changed to _slave
         self._master = self.sentinel.master_for(service_name,
                                                 socket_timeout=timeout)
@@ -160,11 +147,11 @@ class RedisSentinelConnector(BaseRedisConnector):
         self.loop = loop
 
     @property
-    def read_client(self) -> Redis:
+    def read_client(self) -> Valkey:
         return self._slave
 
     @property
-    def write_client(self) -> Redis:
+    def write_client(self) -> Valkey:
         return self._master
 
     async def close(self):
